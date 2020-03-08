4521d547e1c8d6a02fe6d56397e01fa4e065b24c
Ares-X <aresx@protonmail.com>
2020-03-08 17:35:48 +0800

    Fofa-Crawler

---
+++ Fofa-Crawler.py
@@ -0,0 +1,66 @@
+import requests
+import re
+import base64
+import urllib3
+import time
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class GetFoFa():
+    '''
+    @list:存放搜索到的url
+    @max_page:最大搜索页数（普通会员只能搜索10000条，每页10条）
+    @index:当前第几页
+    '''
+    list = []
+    MAX_PAGE = 197
+    domain = 'https://fofa.so'
+    index = 1
+    cookies = {"_fofapro_ars_session": "6d66ad48fda5ce08925bf91c23a41663", "referer_url": "%2Fresult%3Fq%3Dapp%253D%2522Apache-Flink%2522%2B%2526%2526%2Bafter%253D%25222020-01-01%2522%2B%2526%2526%2Bafter%253D%25222020-01-01%2522%26qbase64%3DYXBwPSJBcGFjaGUtRmxpbmsiICYmIGFmdGVyPSIyMDIwLTAxLTAxIiAmJiBhZnRlcj0iMjAyMC0wMS0wMSI%253D"}
+    headers = {"Connection": "close", "Accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01", "Sec-Fetch-Dest": "empty", "X-CSRF-Token": "5OWNi0EU9SDOyHIre2vbJDrHhzrzo7/s5zv8f7R9xChpnldnAKP9/0RhaP5ZaxH0J70meyWADSXOs/eEpygEow==", "X-Requested-With": "XMLHttpRequest", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36", "DNT": "1", "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "cors", "Referer": "https://fofa.so/result?q=app%3D%22Apache-Flink%22+%26%26+after%3D%222020-01-01%22+%26%26+after%3D%222020-01-01%22&qbase64=YXBwPSJBcGFjaGUtRmxpbmsiICYmIGFmdGVyPSIyMDIwLTAxLTAxIiAmJiBhZnRlcj0iMjAyMC0wMS0wMSI%3D", "Accept-Encoding": "gzip, deflate", "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7"}
+
+
+    def __init__(self, key):
+        self.getPage(key)
+
+    def getPage(self, key):
+        #regx=re.compile(r"javascript:view\('(.*)'\)")
+        regx=re.compile(r"javascript:view\(\\'(.*?)\\'\)")  #<a href=\"javascript:view(\'39.106.45.158:8081\')
+        basekey = str(base64.b64encode(key.encode('utf-8')), 'utf-8')
+        while (self.index <= self.MAX_PAGE):
+            url = self.domain + '/result?full=true&page={}&q={}&qbase64={}'.format(self.index, key, basekey)
+            print ("Crawling Page {0}".format(self.index))
+            try:
+                response = requests.get(url, headers=self.headers, cookies=self.cookies)
+            except Exception as err:
+                print(err)
+            target=regx.findall(response.text)
+            for i in target:
+                print(i)
+                self.list.append(i)
+            if len(target)==10:
+                self.index = self.index + 1
+                time.sleep(3)
+                continue
+            else:
+                if response.text == "Retry later\n":
+                    print('请求过于频繁，正在尝试重连-------------------')
+                    time.sleep(10)
+                    continue
+                else:
+                    print('end!-----------------------------------')
+                    break
+
+
+if __name__ == '__main__':
+    #key = input("FOFA KEY:")
+    key = 'app="Apache-Flink"'
+    GetFoFa(key)
+    fileName = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+    with open(key.replace('"','')+fileName + '.txt', 'a+') as f:
+        for i in GetFoFa.list:
+            f.write(i + '\n')
+        f.close()
+    print('Writing over-----------------------------------')
+
